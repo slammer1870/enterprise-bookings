82de50b127bbb74d287902a808b5c863c24a51d0
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-01-14 22:26:41 +0000

    remove: lessons

--- apps/bru-grappling/src/app/(app)/page.tsx
+++ apps/bru-grappling/src/app/(app)/page.tsx
@@ -2,23 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@repo/ui/components/ui/avat
 import { Button } from '@repo/ui/components/ui/button'
 import { Card } from '@repo/ui/components/ui/card'
 
-import { getPayload } from 'payload'
-
-import config from '@payload-config'
-
 export default async function Page() {
-  const payload = await getPayload({
-    config: config,
-  })
-
-  const lessons = await payload.find({
-    collection: 'lessons',
-    limit: 10,
-    depth: 3,
-  })
-
-  console.log(lessons)
-
   return (
     <Card>
       <Button className="bg-red-500">Hello World</Button>
