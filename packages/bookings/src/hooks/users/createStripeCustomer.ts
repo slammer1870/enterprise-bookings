0afde1a9f048bef68b2a3876f533652763379a17
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-01-20 17:06:46 +0000

    update: stripe key

--- packages/bookings/src/hooks/users/createStripeCustomer.ts
+++ packages/bookings/src/hooks/users/createStripeCustomer.ts
@@ -1,7 +1,7 @@
 import type { CollectionBeforeChangeHook } from "payload";
 
 import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
   apiVersion: "2024-12-18.acacia",
 });
 
