f6269a34cf5234fc67cc94eb6280e0887f4ecf77
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-01-21 17:03:05 +0000

    fix: build

--- packages/ui/src/components/ui/calendar.tsx
+++ packages/ui/src/components/ui/calendar.tsx
@@ -58,12 +58,16 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: (props) => {
+          if (props.orientation === "left") {
+            return (
+              <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+            );
+          }
+          return (
+            <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+          );
+        },
       }}
       {...props}
     />
