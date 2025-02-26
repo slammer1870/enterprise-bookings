9dbef0079eef819de62982a4cb4de8321fa622a0
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-02-26 10:38:55 +0000

    add: new migrations

---
+++ apps/bru-grappling/src/migrations/20250226_103846.ts
@@ -0,0 +1,11 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "lessons" ALTER COLUMN "date" SET DEFAULT '2025-02-26T10:38:46.800Z';`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "lessons" ALTER COLUMN "date" SET DEFAULT '2025-02-25T15:04:09.902Z';`)
+}
--- apps/mindful-yard/src/migrations/index.ts
+++ apps/mindful-yard/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250225_150410 from './20250225_150410';
+import * as migration_20250226_103846 from './20250226_103846';
 
 export const migrations = [
   {
     up: migration_20250225_150410.up,
     down: migration_20250225_150410.down,
-    name: '20250225_150410'
+    name: '20250225_150410',
+  },
+  {
+    up: migration_20250226_103846.up,
+    down: migration_20250226_103846.down,
+    name: '20250226_103846'
   },
 ];
