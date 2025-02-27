baf4d5bdf60fda9db1a90b839f533a8155ee2bc9
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-02-27 10:45:51 +0000

    fix: schedule ui

--- packages/bookings/src/components/schedule/lessons/lesson-detail.tsx
+++ packages/bookings/src/components/schedule/lessons/lesson-detail.tsx
@@ -10,42 +10,47 @@ import CheckInButton from "./checkin-button";
 
 export function LessonDetail({ lesson }: { lesson: Lesson }) {
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3" key={lesson.id}>
-      <div className="text-sm font-light">
-        {format(new Date(lesson.startTime), "HH:mm a")} -{" "}
-        {format(new Date(lesson.endTime), "HH:mm a")}
-      </div>
-      <div className="text-xl font-medium">
-        {lesson.classOption.name}{" "}
-        {lesson.location && (
-          <>
-            - <span className="font-normal">{lesson.location}</span>
-          </>
+    <div
+      className="w-full flex flex-col gap-4 md:flex-row md:items-center md:justify-between"
+      key={lesson.id}
+    >
+      <div>
+        <div className="text-sm font-light">
+          {format(new Date(lesson.startTime), "HH:mm a")} -{" "}
+          {format(new Date(lesson.endTime), "HH:mm a")}
+        </div>
+        <div className="text-xl font-medium">
+          {lesson.classOption.name}{" "}
+          {lesson.location && (
+            <>
+              - <span className="font-normal">{lesson.location}</span>
+            </>
+          )}
+        </div>
+        {lesson.user && (
+          <div className="flex items-center justify-start py-2">
+            {lesson.user.image && (
+              <Image
+                src={(lesson.user.image.url as string) || ""}
+                alt={lesson.classOption.name}
+                height={100}
+                width={100}
+                objectFit="cover"
+                className="mr-4 h-12 w-12 rounded-full"
+              />
+            )}
+            <div className="flex flex-col gap-1 items-start justify-start py-2">
+              <span>{lesson.user.name}</span>
+            </div>
+          </div>
+        )}
+        {lesson.bookingStatus !== "closed" && (
+          <span className="font-light text-sm">
+            {lesson.remainingCapacity} places remaining
+          </span>
         )}
       </div>
-      {lesson.user && (
-        <div className="flex items-center justify-start py-2">
-          {lesson.user.image && (
-            <Image
-              src={(lesson.user.image.url as string) || ""}
-              alt={lesson.classOption.name}
-              height={100}
-              width={100}
-              objectFit="cover"
-              className="mr-4 h-12 w-12 rounded-full"
-            />
-          )}
-          <div className="flex flex-col gap-1 items-start justify-start py-2">
-            <span>{lesson.user.name}</span>
-          </div>
-        </div>
-      )}
-      {lesson.bookingStatus !== "closed" && (
-        <span className="font-light text-sm">
-          {lesson.remainingCapacity} places remaining
-        </span>
-      )}
-      <div className="py-2">
+      <div className="w-1/4">
         <CheckInButton lesson={lesson} />
       </div>
     </div>
--- packages/bookings/src/components/schedule/lessons/lesson-list.tsx
+++ packages/bookings/src/components/schedule/lessons/lesson-list.tsx
@@ -6,7 +6,7 @@ import { LessonDetail } from "./lesson-detail";
 
 export function LessonList({ lessons }: { lessons: Lesson[] }) {
   return (
-    <div className="grid gap-4">
+    <div className="flex flex-col gap-4 md:gap-8">
       {lessons && lessons?.length > 0 ? (
         lessons?.map((lesson) => (
           <LessonDetail key={lesson.id} lesson={lesson} />
