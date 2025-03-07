1b2ee5b2ac528ae14a2cdbdfb1a1127dc3ab6f82
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-07 17:51:43 +0000

    add: migrations

---
+++ apps/mindful-yard/src/migrations/20250307_175135.ts
@@ -0,0 +1,13 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "lessons" ALTER COLUMN "date" SET DEFAULT '2025-03-07T17:51:35.517Z';
+  ALTER TABLE "drop_ins_discount_tiers" ALTER COLUMN "min_quantity" SET DEFAULT 1;`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "lessons" ALTER COLUMN "date" SET DEFAULT '2025-03-05T13:39:05.700Z';
+  ALTER TABLE "drop_ins_discount_tiers" ALTER COLUMN "min_quantity" DROP DEFAULT;`)
+}
--- apps/mindful-yard/src/migrations/index.ts
+++ apps/mindful-yard/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250305_133906 from './20250305_133906';
+import * as migration_20250307_175135 from './20250307_175135';
 
 export const migrations = [
   {
     up: migration_20250305_133906.up,
     down: migration_20250305_133906.down,
-    name: '20250305_133906'
+    name: '20250305_133906',
+  },
+  {
+    up: migration_20250307_175135.up,
+    down: migration_20250307_175135.down,
+    name: '20250307_175135'
   },
 ];
