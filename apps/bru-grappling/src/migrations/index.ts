3bb29262cd6ac0278918a0a2a634efcdec6126f0
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-16 18:54:13 +0000

    update: migrations

--- apps/bru-grappling/src/migrations/index.ts
+++ apps/bru-grappling/src/migrations/index.ts
@@ -1,21 +1,9 @@
-import * as migration_20250305_133906 from './20250305_133906';
-import * as migration_20250307_175135 from './20250307_175135';
-import * as migration_20250316_185058 from './20250316_185058';
+import * as migration_20250316_185406 from './20250316_185406';
 
 export const migrations = [
   {
-    up: migration_20250305_133906.up,
-    down: migration_20250305_133906.down,
-    name: '20250305_133906',
-  },
-  {
-    up: migration_20250307_175135.up,
-    down: migration_20250307_175135.down,
-    name: '20250307_175135',
-  },
-  {
-    up: migration_20250316_185058.up,
-    down: migration_20250316_185058.down,
-    name: '20250316_185058'
+    up: migration_20250316_185406.up,
+    down: migration_20250316_185406.down,
+    name: '20250316_185406'
   },
 ];
