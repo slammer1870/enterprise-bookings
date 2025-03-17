9c5abe2cf8109595d20f8183287a7311d88bb713
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-17 15:56:21 +0000

    add: dotenv

--- apps/mindful-yard/src/app/(frontend)/booking-confirmed/page.tsx
+++ apps/mindful-yard/src/app/(frontend)/booking-confirmed/page.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function BookingConfirmedPage() {
   return (
-    <div className="h-[80vh] flex flex-col items-center justify-center">
-      <h2 className="text-2xl font-medium -mt-16 mb-4">Booking confirmed</h2>
+    <div className="h-[80vh] flex flex-col items-center justify-center p-4">
+      <h2 className="text-2xl font-medium -mt-16 mb-2">Booking confirmed</h2>
       <p className="text-sm text-gray-500 mb-2">
         Your booking has been confirmed. Please check your email for the booking details.
       </p>
