2233c805999a70ac3cd77e6c5baf166a5a7b070e
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-18 15:01:37 +0000

    add: improt map

--- apps/mindful-yard/src/app/(payload)/admin/importMap.js
+++ apps/mindful-yard/src/app/(payload)/admin/importMap.js
@@ -1,5 +1,7 @@
 import { FetchLessons as FetchLessons_511233d99c928e84af9e3b44860d4aef } from '@repo/bookings/src/components/lessons/fetch-lessons'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 
 export const importMap = {
-  "@repo/bookings/src/components/lessons/fetch-lessons#FetchLessons": FetchLessons_511233d99c928e84af9e3b44860d4aef
+  "@repo/bookings/src/components/lessons/fetch-lessons#FetchLessons": FetchLessons_511233d99c928e84af9e3b44860d4aef,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e
 }
