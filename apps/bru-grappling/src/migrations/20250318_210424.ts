0dd511f7fb17f6e1db67934ff0a05c0b506d1ee5
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-18 21:04:30 +0000

    migrations

---
+++ apps/bru-grappling/src/migrations/20250318_210424.ts
@@ -0,0 +1,11 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "lessons" ALTER COLUMN "date" SET DEFAULT '2025-03-18T21:04:24.415Z';`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "lessons" ALTER COLUMN "date" SET DEFAULT '2025-03-16T18:54:06.477Z';`)
+}
--- apps/bru-grappling/src/migrations/index.ts
+++ apps/bru-grappling/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250316_185406 from './20250316_185406';
+import * as migration_20250318_210424 from './20250318_210424';
 
 export const migrations = [
   {
     up: migration_20250316_185406.up,
     down: migration_20250316_185406.down,
-    name: '20250316_185406'
+    name: '20250316_185406',
+  },
+  {
+    up: migration_20250318_210424.up,
+    down: migration_20250318_210424.down,
+    name: '20250318_210424'
   },
 ];
