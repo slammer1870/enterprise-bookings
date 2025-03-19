697118507612e4b2f29100d715f0e5a933d99683
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-19 17:17:14 +0000

    add: admin control

--- apps/mindful-yard/src/collections/Users.ts
+++ apps/mindful-yard/src/collections/Users.ts
@@ -1,11 +1,17 @@
 import type { CollectionConfig } from 'payload'
 
+import { checkRole } from '@repo/shared-utils/src/check-role'
+import type { User } from '@repo/shared-types'
+
 export const Users: CollectionConfig = {
   slug: 'users',
   admin: {
     useAsTitle: 'email',
   },
   auth: true,
+  access: {
+    admin: ({ req: { user } }) => checkRole(['admin'], user as User),
+  },
   fields: [
     // Email added by default
     // Add more fields as needed
