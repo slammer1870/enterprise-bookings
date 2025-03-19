4fce1f47a16d6ffeffbcc7b2a8ee1792eb3ce63d
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-19 08:56:53 +0000

    update: revaildiate

--- apps/mindful-yard/src/collections/Pages/hooks/revalidatePage.ts
+++ apps/mindful-yard/src/collections/Pages/hooks/revalidatePage.ts
@@ -9,35 +9,20 @@ export const revalidatePage: CollectionAfterChangeHook<Page> = ({
   previousDoc,
   req: { payload, context },
 }) => {
-  if (!context.disableRevalidate) {
-    if (doc._status === 'published') {
-      const path = doc.slug === 'home' ? '/' : `/${doc.slug}`
+  const path = doc.slug === 'home' ? '/' : `/${doc.slug}`
 
-      payload.logger.info(`Revalidating page at path: ${path}`)
+  payload.logger.info(`Revalidating page at path: ${path}`)
 
-      revalidatePath(path)
-      revalidateTag('pages-sitemap')
-    }
+  revalidatePath(path)
+  revalidateTag('pages-sitemap')
 
-    // If the page was previously published, we need to revalidate the old path
-    if (previousDoc?._status === 'published' && doc._status !== 'published') {
-      const oldPath = previousDoc.slug === 'home' ? '/' : `/${previousDoc.slug}`
-
-      payload.logger.info(`Revalidating old page at path: ${oldPath}`)
-
-      revalidatePath(oldPath)
-      revalidateTag('pages-sitemap')
-    }
-  }
   return doc
 }
 
 export const revalidateDelete: CollectionAfterDeleteHook<Page> = ({ doc, req: { context } }) => {
-  if (!context.disableRevalidate) {
-    const path = doc?.slug === 'home' ? '/' : `/${doc?.slug}`
-    revalidatePath(path)
-    revalidateTag('pages-sitemap')
-  }
+  const path = doc?.slug === 'home' ? '/' : `/${doc?.slug}`
+  revalidatePath(path)
+  revalidateTag('pages-sitemap')
 
   return doc
 }
