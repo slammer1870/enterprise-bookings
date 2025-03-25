6c05a8b9394854d93236515e46801eaf73a8a47c
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-25 13:57:30 +0000

    add: migrations

--- apps/mindful-yard/src/migrations/index.ts
+++ apps/mindful-yard/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250325_135154 from './20250325_135154';
 import * as migration_20250325_135522 from './20250325_135522';
+import * as migration_20250325_135701 from './20250325_135701';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250325_135522.up,
     down: migration_20250325_135522.down,
-    name: '20250325_135522'
+    name: '20250325_135522',
+  },
+  {
+    up: migration_20250325_135701.up,
+    down: migration_20250325_135701.down,
+    name: '20250325_135701'
   },
 ];
