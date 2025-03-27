e9914e34d5424732e217afb4f2ceecadf2f5f2fc
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-27 12:58:23 +0000

    add: more ci

--- apps/bru-grappling/src/migrations/index.ts
+++ apps/bru-grappling/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250327_122753 from './20250327_122753';
 import * as migration_20250327_124523 from './20250327_124523';
+import * as migration_20250327_125708 from './20250327_125708';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250327_124523.up,
     down: migration_20250327_124523.down,
-    name: '20250327_124523'
+    name: '20250327_124523',
+  },
+  {
+    up: migration_20250327_125708.up,
+    down: migration_20250327_125708.down,
+    name: '20250327_125708'
   },
 ];
--- apps/darkhorse-strength/src/migrations/index.ts
+++ apps/darkhorse-strength/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250327_122752 from './20250327_122752';
+import * as migration_20250327_125707 from './20250327_125707';
 
 export const migrations = [
   {
-    up: migration_20250327_122752.up,
-    down: migration_20250327_122752.down,
-    name: '20250327_122752'
+    up: migration_20250327_125707.up,
+    down: migration_20250327_125707.down,
+    name: '20250327_125707'
   },
 ];
