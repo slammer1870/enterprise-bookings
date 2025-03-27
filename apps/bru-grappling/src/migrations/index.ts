099bd6f42696b81394849f8f0efb523ee60c72d5
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-27 12:00:25 +0000

    add: migrations

---
+++ apps/bru-grappling/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250327_115921 from './20250327_115921';
+
+export const migrations = [
+  {
+    up: migration_20250327_115921.up,
+    down: migration_20250327_115921.down,
+    name: '20250327_115921'
+  },
+];
---
+++ apps/darkhorse-strength/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250327_115920 from './20250327_115920';
+
+export const migrations = [
+  {
+    up: migration_20250327_115920.up,
+    down: migration_20250327_115920.down,
+    name: '20250327_115920'
+  },
+];
