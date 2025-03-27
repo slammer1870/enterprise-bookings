1d30428b621d095be14f684b16007ae6b15a550c
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-03-27 12:45:33 +0000

    new migrations

--- apps/bru-grappling/src/migrations/index.ts
+++ apps/bru-grappling/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250327_122753 from './20250327_122753';
+import * as migration_20250327_124523 from './20250327_124523';
 
 export const migrations = [
   {
     up: migration_20250327_122753.up,
     down: migration_20250327_122753.down,
-    name: '20250327_122753'
+    name: '20250327_122753',
+  },
+  {
+    up: migration_20250327_124523.up,
+    down: migration_20250327_124523.down,
+    name: '20250327_124523'
   },
 ];
--- apps/mindful-yard/src/migrations/index.ts
+++ apps/mindful-yard/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250327_122753 from './20250327_122753';
+import * as migration_20250327_124522 from './20250327_124522';
 
 export const migrations = [
   {
     up: migration_20250327_122753.up,
     down: migration_20250327_122753.down,
-    name: '20250327_122753'
+    name: '20250327_122753',
+  },
+  {
+    up: migration_20250327_124522.up,
+    down: migration_20250327_124522.down,
+    name: '20250327_124522'
   },
 ];
