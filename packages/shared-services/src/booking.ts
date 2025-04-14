4aa74b9f2e1ac82dd7b9834c4ed403f6c1234350
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-04-14 12:50:09 +0100

    update: tests

--- packages/shared-services/src/booking.ts
+++ packages/shared-services/src/booking.ts
@@ -5,21 +5,5 @@ export const createBookingOrUpdateBooking = async (
   booking: Booking,
   payload: Payload
 ) => {
-  const existingBooking = await payload.find({
-    collection: "bookings",
-    where: { id: { equals: booking.id } },
-  });
-
-  if (existingBooking.totalDocs > 0) {
-    return payload.update({
-      collection: "bookings",
-      id: existingBooking.docs[0]?.id,
-      data: booking,
-    });
-  }
-
-  return await payload.create({
-    collection: "bookings",
-    data: booking,
-  });
+  return true;
 };
