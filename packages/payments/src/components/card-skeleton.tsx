48a93660eaed169db12a7ca46dd586647079eed2
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-06-17 15:39:13 +0100

    add: card payment form

---
+++ packages/payments/src/components/card-skeleton.tsx
@@ -0,0 +1,47 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@repo/ui/components/ui/card";
+import { Skeleton } from "@repo/ui/components/ui/skeleton";
+
+export default function CardSkeleton() {
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>
+          <Skeleton className="h-4 w-24" />
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-6">
+        {/* Card number skeleton */}
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-24" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+
+        {/* Expiry and CVC skeleton */}
+        <div className="grid grid-cols-2 gap-4">
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-12" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-8" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+        </div>
+
+        <div className="space-y-2">
+          <Skeleton className="h-10 w-full" />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Skeleton className="h-10 w-24 ml-auto" />
+      </CardFooter>
+    </Card>
+  );
+}
