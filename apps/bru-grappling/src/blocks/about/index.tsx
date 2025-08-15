e768ccc29519b4c7a6a9d3f6b7378d8a45e957df
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-08-15 18:09:31 +0100

    fix: layout

--- apps/bru-grappling/src/blocks/about/index.tsx
+++ apps/bru-grappling/src/blocks/about/index.tsx
@@ -1,6 +1,6 @@
 import { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
 import { RichText } from '@payloadcms/richtext-lexical/react'
-import Image from "next/image"
+import Image from 'next/image'
 
 export const AboutBlock: React.FC<{
   sections: {
@@ -21,22 +21,20 @@ export const AboutBlock: React.FC<{
           key={index}
           className="relative flex flex-col items-center justify-center py-20 lg:min-h-screen lg:flex-row"
         >
-          <div
-            className={`container mx-auto mb-12 px-4 ${section.imagePosition === 'right' ? '' : 'lg:ml-auto'}`}
-          >
-            <div
-              className={`${section.imagePosition === 'right' ? 'lg:mr-auto' : 'lg:ml-auto'} lg:w-2/5`}
-            >
-              <div>
-                <h3 className="mb-8 text-2xl font-medium uppercase text-gray-800 lg:text-3xl">
-                  {section.title}
-                </h3>
-                <RichText data={section.content} className="prose prose-a:text-blue-500" />
-              </div>
+          <div className={`container mx-auto mb-12 px-4 ${section.imagePosition === 'left' ? 'lg:flex' : ''}`}>
+            <div className={`${section.imagePosition === 'left' ? 'lg:ml-auto lg:w-2/3 xl:w-auto' : ''}`}>
+              <h3 className="mb-8 text-2xl font-medium uppercase text-gray-800 lg:text-3xl">
+                {section.title}
+              </h3>
+              <RichText data={section.content} className="prose prose-a:text-blue-500 [&_p]:mb-4 [&_p]:lg:text-xl" />
             </div>
           </div>
           <div
-            className={`lg:top-0 ${section.imagePosition === 'right' ? 'ml-auto lg:right-0' : 'mr-auto lg:left-0'} w-2/3 lg:absolute lg:w-2/5 2xl:w-auto`}
+            className={`lg:top-50 w-2/3 lg:absolute lg:w-1/3 2xl:w-auto ${
+              section.imagePosition === 'right' 
+                ? 'ml-auto lg:right-0' 
+                : 'mr-auto lg:left-0'
+            }`}
           >
             <div>
               <Image
@@ -45,14 +43,13 @@ export const AboutBlock: React.FC<{
                 height={section.image.height}
                 alt={section.title}
                 style={{
-                  maxWidth: "100%",
-                  height: "auto",
-                  objectFit: "cover"
-                }} />
+                  objectFit: 'cover',
+                }}
+              />
             </div>
           </div>
         </section>
       ))}
     </>
-  );
+  )
 }
