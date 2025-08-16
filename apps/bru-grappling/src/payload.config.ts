30d6c214d1803b1d8d105545450036d5344909fe
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-08-16 15:47:19 +0100

    fix: zod

--- apps/bru-grappling/src/payload.config.ts
+++ apps/bru-grappling/src/payload.config.ts
@@ -1,6 +1,6 @@
 // storage-adapter-import-placeholder
 import { postgresAdapter } from '@payloadcms/db-postgres'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
+// import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { seoPlugin } from '@payloadcms/plugin-seo'
 import { resendAdapter } from '@payloadcms/email-resend'
