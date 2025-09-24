7826d8dfaec30342ca90676e4423b4f74bde1b50
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-09-24 12:10:43 +0100

    fix: email access

--- packages/auth/src/fields/email.ts
+++ packages/auth/src/fields/email.ts
@@ -1,3 +1,4 @@
+import { checkRole } from "@repo/shared-utils";
 import { Field } from "payload";
 
 export const email: Field = {
@@ -6,4 +7,20 @@ export const email: Field = {
   type: "text",
   required: true,
   unique: true,
+  access: {
+    read: ({ req: { user }, siblingData }) => {
+      console.log("FIELD ACCESS", siblingData);
+      console.log("USER", user);
+
+      if (siblingData?.id === user?.id) {
+        return true;
+      }
+
+      if (siblingData?.parent && siblingData?.parent === user?.id) {
+        return true;
+      }
+
+      return false;
+    },
+  },
 };
