76a663c270f88d1c320a0722402ea54b98fc2030
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-11-25 19:21:08 +0000

    update: subscripin unique

--- apps/darkhorse-strength/src/migrations/index.ts
+++ apps/darkhorse-strength/src/migrations/index.ts
@@ -3,6 +3,7 @@ import * as migration_20250722_163214 from './20250722_163214';
 import * as migration_20250722_163320 from './20250722_163320';
 import * as migration_20250723_110130 from './20250723_110130';
 import * as migration_20250725_195653 from './20250725_195653';
+import * as migration_20251125_191947 from './20251125_191947';
 
 export const migrations = [
   {
@@ -28,6 +29,11 @@ export const migrations = [
   {
     up: migration_20250725_195653.up,
     down: migration_20250725_195653.down,
-    name: '20250725_195653'
+    name: '20250725_195653',
+  },
+  {
+    up: migration_20251125_191947.up,
+    down: migration_20251125_191947.down,
+    name: '20251125_191947'
   },
 ];
