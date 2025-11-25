af08326de01cb1a06b145c2cf33d0d01011fc54d
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-11-25 16:27:18 +0000

    fix: modal

--- apps/bru-grappling/src/app/(frontend)/complete-booking/page.tsx
+++ apps/bru-grappling/src/app/(frontend)/complete-booking/page.tsx
@@ -1,14 +1,9 @@
 import { RegisterLoginTabs } from '@repo/auth-next'
-import { Modal } from '../modal'
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
 
 export default async function CompleteBooking({ searchParams }: { searchParams: SearchParams }) {
   const { mode = 'login' } = await searchParams
 
-  return (
-    <Modal>
-      <RegisterLoginTabs value={mode as 'login' | 'register'} />
-    </Modal>
-  )
+  return <RegisterLoginTabs value={mode as 'login' | 'register'} />
 }
