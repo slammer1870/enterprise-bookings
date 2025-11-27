74397e440c07fc27a4e5d52647986864f1859e9d
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-11-27 12:37:54 +0000

    fix new migrations

--- apps/bru-grappling/src/migrations/index.ts
+++ apps/bru-grappling/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251123_131951 from './20251123_131951'
 import * as migration_20251125_191947 from './20251125_191947'
+import * as migration_20251127_120004 from './20251127_120004'
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20251125_191947.up,
     down: migration_20251125_191947.down,
     name: '20251125_191947',
+  },
+  {
+    up: migration_20251127_120004.up,
+    down: migration_20251127_120004.down,
+    name: '20251127_120004',
   },
 ]
