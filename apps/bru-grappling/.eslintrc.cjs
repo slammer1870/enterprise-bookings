836b82101b0b6f1fd608598fe6f7c2fde21d40b0
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-11-27 12:51:22 +0000

    add: standard uslint

--- apps/bru-grappling/.eslintrc.cjs
+++ apps/bru-grappling/.eslintrc.cjs
@@ -1,36 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['next/core-web-vitals', 'next/typescript'],
+  extends: ['@repo/eslint-config/next.js'],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
   rules: {
-    '@typescript-eslint/ban-ts-comment': 'warn',
-    '@typescript-eslint/no-empty-object-type': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unsafe-argument': 'warn',
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        args: 'after-used',
-        ignoreRestSiblings: false,
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        destructuredArrayIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^(_|ignore)',
-      },
-    ],
     '@next/next/no-duplicate-head': 'off',
   },
   ignorePatterns: ['.next/', 'src/app/(payload)/admin/importMap.js'],
-}
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['next/core-web-vitals'],
-  parserOptions: {
-    project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname,
-  },
 }
