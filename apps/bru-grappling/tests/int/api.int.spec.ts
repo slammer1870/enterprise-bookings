03085038e8036751d1d3892dbace1d917115e98e
Sam Mc Nally <sam.mcnally94@gmail.com>
2025-12-02 09:47:32 +0000

    add: bru testing

---
+++ apps/bru-grappling/tests/int/api.int.spec.ts
@@ -0,0 +1,27 @@
+import { getPayload, Payload } from 'payload'
+import config from '@/payload.config'
+
+import { describe, it, beforeAll, afterAll, expect } from 'vitest'
+
+let payload: Payload
+
+describe('API', () => {
+  beforeAll(async () => {
+    const payloadConfig = await config
+    payload = await getPayload({ config: payloadConfig })
+  })
+
+  afterAll(async () => {
+    if (payload) {
+      await payload.db.destroy()
+    }
+  })
+
+  it('fetches users', async () => {
+    const users = await payload.find({
+      collection: 'users',
+    })
+    expect(users).toBeDefined()
+  })
+})
+
