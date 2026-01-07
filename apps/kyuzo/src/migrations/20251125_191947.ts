f321e39aaf52750157ec1b654ffefcc83db8a713
Sam Mc Nally <sam.mcnally94@gmail.com>
2026-01-07 10:53:21 +0000

    fix: kyuzo igrsation

--- apps/kyuzo/src/migrations/20251125_191947.ts
+++ apps/kyuzo/src/migrations/20251125_191947.ts
@@ -2,11 +2,11 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   DROP INDEX "subscriptions_stripe_subscription_id_idx";
+   DROP INDEX IF EXISTS "subscriptions_stripe_subscription_id_idx";
   ALTER TABLE "lessons" ALTER COLUMN "date" SET DEFAULT '2025-11-25T19:19:46.884Z';
   ALTER TABLE "scheduler_week_days_time_slot" ALTER COLUMN "start_time" SET DEFAULT '2025-11-25T19:19:47.042Z';
   ALTER TABLE "scheduler_week_days_time_slot" ALTER COLUMN "end_time" SET DEFAULT '2025-11-25T19:19:47.042Z';
-  ALTER TABLE "lessons" ADD COLUMN "active" boolean DEFAULT true;`)
+  ALTER TABLE "lessons" ADD COLUMN IF NOT EXISTS "active" boolean DEFAULT true;`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
@@ -14,6 +14,6 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
    ALTER TABLE "lessons" ALTER COLUMN "date" SET DEFAULT '2025-07-25T20:02:22.923Z';
   ALTER TABLE "scheduler_week_days_time_slot" ALTER COLUMN "start_time" SET DEFAULT '2025-07-25T20:02:23.082Z';
   ALTER TABLE "scheduler_week_days_time_slot" ALTER COLUMN "end_time" SET DEFAULT '2025-07-25T20:02:23.082Z';
-  CREATE UNIQUE INDEX "subscriptions_stripe_subscription_id_idx" ON "subscriptions" USING btree ("stripe_subscription_id");
-  ALTER TABLE "lessons" DROP COLUMN "active";`)
+  CREATE UNIQUE INDEX IF NOT EXISTS "subscriptions_stripe_subscription_id_idx" ON "subscriptions" USING btree ("stripe_subscription_id");
+  ALTER TABLE "lessons" DROP COLUMN IF EXISTS "active";`)
 }
