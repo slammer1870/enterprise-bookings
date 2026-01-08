30b4cead943d8db78fec9bed65fb6be173386ce6
Sam Mc Nally <sam.mcnally94@gmail.com>
2026-01-08 17:42:28 +0000

    try: last css secelctor

--- apps/kyuzo/src/app/(frontend)/layout.tsx
+++ apps/kyuzo/src/app/(frontend)/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import './globals.css'
 import '@repo/ui/globals.css'
+import './globals.css'
 import '@daveyplate/better-auth-ui/css'
 
 import { Navbar } from '@/globals/navbar'
