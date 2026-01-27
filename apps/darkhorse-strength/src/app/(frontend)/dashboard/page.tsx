a44f65bc60e1412f51aab0cbadf9ca8763f0e31b
Sam Mc Nally <sam.mcnally94@gmail.com>
2026-01-27 08:32:37 +0000

    fix page auth

--- apps/darkhorse-strength/src/app/(frontend)/dashboard/page.tsx
+++ apps/darkhorse-strength/src/app/(frontend)/dashboard/page.tsx
@@ -7,11 +7,12 @@ import config from '@payload-config'
 import type { Plan, Subscription } from '@repo/shared-types'
 
 import { DashboardMembershipPanel } from '@/components/dashboard-membership-panel.client'
-import { currentUser } from '@/lib/auth/context/get-context-props'
+import { getSession } from '@/lib/auth/context/get-context-props'
 import { redirect } from 'next/navigation'
 
 export default async function Dashboard() {
-  const user = await currentUser()
+  const session = await getSession()
+  const user = session?.user
   if (!user) {
     redirect('/auth/sign-in?callbackUrl=/dashboard')
   }
@@ -23,7 +24,7 @@ export default async function Dashboard() {
     where: {
       and: [
         {
-          user: { equals: user.id },
+          user: { equals: user?.id },
           status: { not_in: ['canceled', 'unpaid', 'incomplete_expired', 'incomplete'] },
           endDate: { greater_than: new Date() },
         },
@@ -45,7 +46,7 @@ export default async function Dashboard() {
   return (
     <div className="container mx-auto pt-24 px-4 min-h-screen">
       <h1 className="text-3xl font-bold">Dashboard</h1>
-      <p className="text-sm text-gray-500">Welcome {user.name}</p>
+      <p className="text-sm text-gray-500">Welcome {user?.name}</p>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="max-w-screen-sm w-full mx-auto p-6" id="schedule">
           <h2 className="text-2xl font-medium text-center mb-4">Schedule</h2>
