39efae1fff7b70041d1c984977f47ebb19782845
Sam Mc Nally <sam.mcnally94@gmail.com>
2026-02-11 13:17:44 +0000

    fix: stripe versioning

--- apps/atnd-me/src/lib/stripe/index.ts
+++ apps/atnd-me/src/lib/stripe/index.ts
@@ -3,7 +3,5 @@
 import Stripe from 'stripe'
 
 export const stripe = process.env.STRIPE_SECRET_KEY
-  ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2025-02-24.acacia',
-    })
+  ? new Stripe(process.env.STRIPE_SECRET_KEY)
   : undefined
