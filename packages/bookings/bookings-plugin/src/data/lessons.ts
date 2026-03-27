a22837404c6d08d171f1c52e8ed8768fb121d276
Sam Mc Nally <sam.mcnally94@gmail.com>
2026-03-27 12:42:38 +0000

    fix admin lesson redirect

--- packages/bookings/bookings-plugin/src/data/lessons.ts
+++ packages/bookings/bookings-plugin/src/data/lessons.ts
@@ -1,5 +1,4 @@
 import { BasePayload, PayloadRequest } from "payload";
-import { redirect } from "next/navigation";
 
 import { getLessonsQuery } from "@repo/shared-utils";
 import { Lesson } from "@repo/shared-types";
@@ -15,14 +14,11 @@ export const getLessons = async (
   const startQuery = "where[or][0][and][0][startTime][greater_than_equal]";
 
   const condition = searchParams && searchParams[startQuery];
+  const effectiveSearchParams = condition
+    ? searchParams
+    : getLessonsQuery(new Date()).replace(/^\?/, "");
 
-  if (!condition) {
-    const query = getLessonsQuery(new Date());
-
-    redirect(`/admin/collections/lessons${query}`);
-  }
-
-  const ps = qs.parse(searchParams as unknown as string, {
+  const ps = qs.parse(effectiveSearchParams as unknown as string, {
     ignoreQueryPrefix: true,
     depth: 6,
   });
